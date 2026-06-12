ed176e9274227b25c141a09cafbc19417d640746
Premchand Premchand <premchand@pranathibs.local>
2026-06-12 17:55:14 +0530

    Configured media files handling

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT= BASE_DIR /'static'
 STATICFILES_DIRS = ['mysite/static']
+
+#Media files configuration
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
